b9c2bf4ca4e772e83f9ad757a3cb1a53221c2c56
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-04 13:53:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const changeText = () => {
+  document.getElementById('text').innerHTML = 'This is really cool!'
+}
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  changeText()
+})
