00a54bcf4ad4b20a3e215d4806a026b6df265068
pharia-le <pharia.le@gmail.com>
2020-06-08 02:05:12 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+function replaceText() {
+    document.getElementById("text").innerHTML = "This is really cool!"
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    replaceText();
+});
